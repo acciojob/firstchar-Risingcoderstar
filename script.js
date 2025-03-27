10b914debada0628dd3af07f8fa284537676fabd
Risingcoderstar <94625582+Risingcoderstar@users.noreply.github.com>
2025-03-27 14:49:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstChar(text) {
   // your code here
+let n=text.length;
+let i=0;
+let res='';
+text.forEach((ch)=>{
+if(ch!=='')
+{
+return ch;
+}
+})
+return res;
 }
 
 // Do not change the code below
